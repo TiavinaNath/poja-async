7aebe2a241b56ba6382686b00f2d6d54e59a036f
TiavinaNath <hei.tiavina.3@gmail.com>
2026-03-18 10:54:43 +0300

    feat: async email sending

--- src/main/java/api/poja/app/endpoint/rest/controller/HelloWorldController.java
+++ src/main/java/api/poja/app/endpoint/rest/controller/HelloWorldController.java
@@ -1,8 +1,7 @@
 package api.poja.app.endpoint.rest.controller;
 
-import api.poja.app.mail.Email;
-import api.poja.app.mail.Mailer;
-import jakarta.mail.internet.InternetAddress;
+import api.poja.app.endpoint.event.EventProducer;
+import api.poja.app.endpoint.event.model.SendEmailRequested;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.SneakyThrows;
@@ -13,16 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @AllArgsConstructor
 public class HelloWorldController {
-  private final Mailer mailer;
+  private final EventProducer<SendEmailRequested> eventProducer;
 
   @GetMapping("/hello")
   @SneakyThrows
   public String helloWorld(@RequestParam String to) {
-    var email =
-        new Email(
-            new InternetAddress(to), List.of(), List.of(), "Hello world", "... world!", List.of());
-
-    mailer.accept(email);
+    var event = SendEmailRequested.builder().to(to).build();
+    eventProducer.accept(List.of(event));
     return "... world!";
   }
 }
